d2a3e40335309e219190fa3f9c0b6f62c510571a
Du Juan <jdu.xian@gmail.com>
2020-10-29 15:49:54 +0800

    feedback by Du Juan

--- src/App/App.js
+++ src/App/App.js
@@ -3,10 +3,14 @@ import './App.scss';
 import GroupDisplay from '../components/GroupDisplay/GroupDisplay';
 import MemberDisplay from '../components/MemberDisplay/MemberDisplay';
 
+// TODO GTB-工程实践: - 没有小步提交
+// TODO GTB-完成度: * 没有联调后端，功能没有实现
+// TODO GTB-完成度: * 完成度太低，知识点运用和工程实践没法验证
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
+        {/* TODO GTB-工程实践: - 列表命名为GroupList比较合理 */}
         <GroupDisplay />
         <MemberDisplay />
       </div>
--- src/components/GroupDisplay/GroupDisplay.js
+++ src/components/GroupDisplay/GroupDisplay.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './GroupDisplay.css';
 
+// TODO GTB-工程实践: - css class命名不符合规范，不建议用驼峰
+// TODO GTB-工程实践: - 命名不符合业务功能，看不出来职责，命名应该更贴合业务或功能
 class GroupDisplay extends React.Component {
   render() {
     return (
       <div className="groupBody">
+        {/* TODO GTB-工程实践: - nav标签不太合理，header更符合 */}
         <nav className="nav">
           <h3>分组列表</h3>
           <button type="button" className="groupButton">
@@ -12,6 +15,7 @@ class GroupDisplay extends React.Component {
           </button>
         </nav>
         <div className="tableContainer">
+          {/* TODO GTB-知识点: - 用table标签不合理 */}
           <table className="groupTable">
             <thead className="thead">1组</thead>
             <div className="tableBody">
